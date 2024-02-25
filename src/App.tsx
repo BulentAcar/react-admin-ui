fb51f2aeff3b320319c2f18cfae3eef30a258d8a
BulentAcar <bacarnl@hotmail.com>
2024-02-25 21:50:51 +0100

    Register Page working
    Firebase is working
    Register page is working

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,30 @@ import Login from "./pages/authentication/Login";
 import "./styles/global.scss";
 import User from "./pages/user/User";
 import Product from "./pages/product/Product";
-import Register from "./pages/authentication/register";
+import Register from "./pages/authentication/Register";
 import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
 
 
 const queryClient = new QueryClient();
--- src/pages/authentication/Register.tsx
+++ src/pages/authentication/Register.tsx
@@ -1,29 +1,97 @@
-import React from "react"
+import React, { useState } from "react"
 import "./register.scss"
 import avatar from "../../assets/images/addAvatar.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth"
+import { getAuth } from "firebase/auth"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { getFirestore, doc, setDoc } from "firebase/firestore"; 
+import { Link, Navigate } from "react-router-dom"
+
+
+
+
 
 const Register = () => {
-  return (
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const auth = getAuth();
+  const storage = getStorage();
+  const db = getFirestore();
+  
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true);
+    e.preventDefault();
+    const displayName = e.currentTarget[0].value;
+    const email = e.currentTarget[1].value;
+    const password = e.currentTarget[2].value;
+    const file = e.currentTarget[3].files[0];
+    console.log(displayName, email, password, file);
     
-          <div className="formcontainer">
-            <div className="formwrapper">
-              <span className="logo">Dashboard</span>
-              <span className="title">Register</span>
-              <form>
-                <input type="text" placeholder="Display Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input style={{display:"none"}} type="file" id="file" />
-                <label htmlFor="file">
-                  <img src={avatar} alt="" />
-                  <span>Add an avatar</span>
-                </label>
-                <button>Register</button>
-              </form>
-              <p>Already have an account? Login</p>
-            </div>
-          </div>    
-  )
-}
+    
+    try {
+      //Create user
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
-export default Register
+      //Create a unique image name
+      const date = new Date().getTime();
+      const storageRef = ref(storage, `${displayName + date}`);
+
+      await uploadBytesResumable(storageRef, file).then(() => {
+        getDownloadURL(storageRef).then(async (downloadURL) => {
+          try {
+            //Update profile
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            //create user on firestore
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+
+            //create empty user chats on firestore
+            await setDoc(doc(db, "userChats", res.user.uid), {});
+            
+          } catch (err) {
+            console.log(err);
+            setError(true);
+            setLoading(false);
+          }
+        });
+      });
+    } catch (err) {
+      setError(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">DASHBOARD</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input required type="text" placeholder="display name" />
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <input required style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={avatar} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button disabled={loading}>Sign up</button>
+          {loading && "Uploading and compressing the image please wait..."}
+          {error && <span>Something went wrong</span>}
+        </form>
+        <p>
+          You do have an account? <Link to="/register">Login</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
